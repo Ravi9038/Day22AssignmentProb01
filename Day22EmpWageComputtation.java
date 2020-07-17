4536ef77e2cd5ae9821d54b32580756c7a4de324
ravi9038 <ravijadhav9038@gmail.com>
2020-07-17 10:03:26 +0530

    Changes in UC5

--- Day22EmpWageComputtation.java
+++ Day22EmpWageComputtation.java
@@ -2,31 +2,38 @@
 
 public class Day22EmpWageComputtation{
 
-	public static final int isPartTime = 1;
-	public static final int isFullTime = 2;
-	public static final int empRatePerHr=20;
+        public static final int isPartTime = 1;
+        public static final int isFullTime = 2;
+        public static final int empRatePerHr=20;
+        public static final int numOfWorkingDays=20;
 
-	public static void main(String [] args){
+        public static void main(String [] args){
 
+                int empHrs = 0;
+                int empWage = 0;
+                int totalEmpWage = 0;
 
-		int empHrs = 0;
-		int empWage = 0;
+                for (int day=1; day<=numOfWorkingDays; day++){
 
-		int empCheck = (int) Math.floor(Math.random()*10) % 3;
+                        int empCheck = (int) Math.floor(Math.random()*10) % 3;
 
-		switch(empCheck){
+                        switch(empCheck){
 
-			case isPartTime :
-				empHrs = 4;
-				break;
-			case isFullTime :
-				empHrs = 8;
-				break;
+                                case isPartTime :
+                                        empHrs = 4;
+                                        break;
+                                case isFullTime :
+                                        empHrs = 8;
+                                        break;
 
-			default :
-				empHrs = 0;
-		}
-		empWage = empHrs * empRatePerHr;
-		System.out.println("Emp Wage "+ empWage);
-	}
+                                default :
+                                        empHrs = 0;
+                        }
+                        empWage += empHrs * empRatePerHr;
+                        totalEmpWage += empWage;
+                        System.out.println("Emp Wage : "+empWage);
+                }
+
+                System.out.println("Total Emp Wage "+ totalEmpWage);
+        }
 }
